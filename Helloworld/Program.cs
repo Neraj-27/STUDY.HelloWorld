e5806cab7dacc755d0dacbf6375ebd9a1f282099
Neraj-27 <nerajaksharaju27@gmail.com>
2026-03-28 13:34:06 +0530

    Initial Commit

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -1,9 +1,16 @@
-﻿Console.WriteLine("Hello, C# Academy! One more Modification");
-Console.WriteLine("Hello, C# Academy! One more Modification");
-Console.WriteLine("Hello, C# Academy! One more Modification");
-Console.WriteLine("Hello, C# Academy! One more Modification");
-Console.WriteLine("Hello, C# Academy! One more Modification");
-Console.WriteLine("Hello, C# Academy! One more Modification");
-Console.WriteLine("Hello, C# Academy! One more Modification");
-Console.WriteLine("Hello, C# Academy! One more Modification");
-Console.WriteLine("Hello, C# Academy! One more Modification");
+﻿MyFirstMethod();
+
+void MyFirstMethod(){
+    Console.WriteLine("1. Hello World!");
+}
+
+MySecondMethod();
+
+void MySecondMethod(){
+    Console.WriteLine("2. Hello World!");
+}
+
+MyThridMethod();
+void MyThridMethod(){
+    Console.WriteLine("3. Hello World!");
+}
